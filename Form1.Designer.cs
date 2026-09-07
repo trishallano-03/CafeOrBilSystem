2f1b87c6ff7842e1db53d5495ea1c811b45807b3
student <student@DESKTOP-RH5DKHC>
2026-09-07 15:20:53 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,452 @@
+﻿namespace CafeOrBilSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox7 = new CheckBox();
+            checkBox8 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            groupBox3 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            checkBox9 = new CheckBox();
+            groupBox4 = new GroupBox();
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown3 = new NumericUpDown();
+            numericUpDown4 = new NumericUpDown();
+            groupBox5 = new GroupBox();
+            numericUpDown5 = new NumericUpDown();
+            numericUpDown6 = new NumericUpDown();
+            numericUpDown7 = new NumericUpDown();
+            numericUpDown8 = new NumericUpDown();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox6 = new GroupBox();
+            txtPayment = new TextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown8).BeginInit();
+            groupBox6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(15, 22);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(112, 19);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Burger   (₱85.00)";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(37, 71);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(195, 160);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Foods";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(checkBox5);
+            groupBox2.Controls.Add(checkBox6);
+            groupBox2.Controls.Add(checkBox7);
+            groupBox2.Controls.Add(checkBox8);
+            groupBox2.Location = new Point(331, 71);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(195, 160);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Drinks";
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(15, 134);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(152, 19);
+            checkBox5.TabIndex = 3;
+            checkBox5.Text = "Bottled Water    (₱25.00)";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(15, 57);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(119, 19);
+            checkBox6.TabIndex = 1;
+            checkBox6.Text = "Iced Tea   (₱40.00)";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(15, 94);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(112, 19);
+            checkBox7.TabIndex = 2;
+            checkBox7.Text = "Coffee   (₱45.00)";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(15, 22);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(129, 19);
+            checkBox8.TabIndex = 1;
+            checkBox8.Text = "Soft Drink   (₱35.00)";
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(15, 134);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(143, 19);
+            checkBox4.TabIndex = 3;
+            checkBox4.Text = "French Fries    (₱50.00)";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(15, 57);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(174, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Chicked Sandwich   (₱75.00)";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(15, 94);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(130, 19);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "Spaghetti    (₱95.00)";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Georgia", 16F, FontStyle.Italic);
+            label1.Location = new Point(226, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(343, 27);
+            label1.TabIndex = 3;
+            label1.Text = "Café Ordering and Billing System";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(6, 18);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(56, 23);
+            numericUpDown1.TabIndex = 4;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(radioButton3);
+            groupBox3.Controls.Add(radioButton2);
+            groupBox3.Controls.Add(radioButton1);
+            groupBox3.Location = new Point(37, 255);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(108, 100);
+            groupBox3.TabIndex = 5;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Order Type";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(15, 22);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(64, 19);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Dine-in";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(15, 47);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(66, 19);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Takeout";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(15, 72);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(67, 19);
+            radioButton3.TabIndex = 8;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Delivery";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(16, 22);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(148, 19);
+            checkBox9.TabIndex = 6;
+            checkBox9.Text = "Senior Citizen Discount";
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(numericUpDown4);
+            groupBox4.Controls.Add(numericUpDown3);
+            groupBox4.Controls.Add(numericUpDown2);
+            groupBox4.Controls.Add(numericUpDown1);
+            groupBox4.Location = new Point(238, 71);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(69, 160);
+            groupBox4.TabIndex = 8;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Quantity";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(6, 53);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(56, 23);
+            numericUpDown2.TabIndex = 5;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(6, 90);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(56, 23);
+            numericUpDown3.TabIndex = 6;
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Location = new Point(6, 130);
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(56, 23);
+            numericUpDown4.TabIndex = 7;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(numericUpDown5);
+            groupBox5.Controls.Add(numericUpDown6);
+            groupBox5.Controls.Add(numericUpDown7);
+            groupBox5.Controls.Add(numericUpDown8);
+            groupBox5.Location = new Point(532, 71);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(69, 160);
+            groupBox5.TabIndex = 9;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Quantity";
+            // 
+            // numericUpDown5
+            // 
+            numericUpDown5.Location = new Point(6, 130);
+            numericUpDown5.Name = "numericUpDown5";
+            numericUpDown5.Size = new Size(56, 23);
+            numericUpDown5.TabIndex = 7;
+            // 
+            // numericUpDown6
+            // 
+            numericUpDown6.Location = new Point(6, 90);
+            numericUpDown6.Name = "numericUpDown6";
+            numericUpDown6.Size = new Size(56, 23);
+            numericUpDown6.TabIndex = 6;
+            // 
+            // numericUpDown7
+            // 
+            numericUpDown7.Location = new Point(6, 53);
+            numericUpDown7.Name = "numericUpDown7";
+            numericUpDown7.Size = new Size(56, 23);
+            numericUpDown7.TabIndex = 5;
+            // 
+            // numericUpDown8
+            // 
+            numericUpDown8.Location = new Point(6, 18);
+            numericUpDown8.Name = "numericUpDown8";
+            numericUpDown8.Size = new Size(56, 23);
+            numericUpDown8.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(151, 273);
+            button1.Name = "button1";
+            button1.Size = new Size(108, 23);
+            button1.TabIndex = 10;
+            button1.Text = "Calculate Order";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(151, 302);
+            button2.Name = "button2";
+            button2.Size = new Size(108, 23);
+            button2.TabIndex = 11;
+            button2.Text = "Process Payment";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(151, 332);
+            button3.Name = "button3";
+            button3.Size = new Size(108, 23);
+            button3.TabIndex = 12;
+            button3.Text = "Clear Order";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(checkBox9);
+            groupBox6.Location = new Point(330, 302);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(168, 53);
+            groupBox6.TabIndex = 14;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "Discount";
+            // 
+            // txtPayment
+            // 
+            txtPayment.Location = new Point(120, 383);
+            txtPayment.Name = "txtPayment";
+            txtPayment.Size = new Size(100, 23);
+            txtPayment.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(634, 552);
+            Controls.Add(txtPayment);
+            Controls.Add(groupBox6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown8).EndInit();
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private CheckBox checkBox1;
+        private GroupBox groupBox1;
+        private Label label1;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox4;
+        private NumericUpDown numericUpDown1;
+        private GroupBox groupBox2;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox7;
+        private CheckBox checkBox8;
+        private GroupBox groupBox3;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private CheckBox checkBox9;
+        private GroupBox groupBox4;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown2;
+        private GroupBox groupBox5;
+        private NumericUpDown numericUpDown5;
+        private NumericUpDown numericUpDown6;
+        private NumericUpDown numericUpDown7;
+        private NumericUpDown numericUpDown8;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private GroupBox groupBox6;
+        private TextBox txtPayment;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace CafeOrBilSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
